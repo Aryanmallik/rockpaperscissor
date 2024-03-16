188d5c955d0444794b3ef622283bb011b49d0ae6
Aryanmallik <aryanmallik2004@gmail.com>
2024-03-16 18:24:43 +0530

    merge

--- a.js
+++ a.js
@@ -32,11 +32,78 @@ function computeWinner(a, b) {
     return 2;
   }
 }
+// function computeScore() {
+//   let uscore = 0;
+//   let cscore = 0;
+//   let gameOver = false;
+//   const buttons = document.querySelectorAll(".ch");
+//   const playerScore = document.getElementById("playerScore");
+//   const cpuScore = document.getElementById("cpuScore");
+//   const result = document.getElementById("result");
+//   const player = document.getElementById("player");
+
+//   buttons.forEach((button) => {
+//     button.addEventListener("click", function () {
+//       if (gameOver) return;
+
+//       let a = this.value;
+//       console.log("user-", a);
+//       if (a == "rock") {
+//         player.innerHTML = `<img src="./Public/rock.png" height="200px">`;
+//       } else if (a == "paper") {
+//         player.innerHTML = `<img src="./Public/paper.png" height="200px">`;
+//       } else if (a == "scissors") {
+//         player.innerHTML = `<img src="./Public/scissor.png" height="200px">`;
+//       }
+
+//       let b = compinp();
+//       let val = computeWinner(a, b);
+
+//       if (val === 0) {
+//         console.log("draw");
+//         result.innerHTML = "draw";
+//       } else if (val == 1) {
+//         cscore++;
+//         result.innerHTML = "you lose";
+//       } else if (val == 2) {
+//         uscore++;
+//         result.innerHTML = "you win";
+//       }
+//       playerScore.innerHTML = uscore;
+//       cpuScore.innerHTML = cscore;
+//       console.log("user-", uscore, "comp-", cscore);
+
+//       if (uscore == 5) {
+//         console.log("user win");
+//         result.innerHTML =
+//           "game over you win<br/><br/>click one of the options to restart";
+//         gameOver = true;
+//       } else if (cscore == 5) {
+//         result.innerHTML =
+//           "game over you lose<br/><br/>click one of the options to restart";
+//         gameOver = true;
+//       }
+//     });
+//   });
+
+//   buttons.forEach((button) => {
+//     button.addEventListener("click", function () {
+//       uscore = 0;
+//       cscore = 0;
+//       gameOver = false;
+//       playerScore.innerHTML = uscore;
+//       cpuScore.innerHTML = cscore;
+//       result.innerHTML = "";
+//       player.innerHTML = ``;
+//       comp.innerHTML = ``;
+//     });
+//   });
+// }
 
 function computeScore() {
   let uscore = 0;
   let cscore = 0;
-  let gameOver = 0;
+  let gameOver = false;
   const buttons = document.querySelectorAll(".ch");
   buttons.forEach((button) => {
     button.addEventListener("click", function () {
@@ -84,12 +151,13 @@ function computeScore() {
           button.addEventListener("click", function () {
             uscore = 0;
             cscore = 0;
-            gameOver = 1;
+            gameOver = false;
             playerScore.innerHTML = uscore;
             cpuScore.innerHTML = cscore;
             result.innerHTML = "";
             player.innerHTML = ``;
             comp.innerHTML = ``;
+            location.reload();
           });
         });
       }
