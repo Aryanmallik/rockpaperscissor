a06d3d1201791e70c6a78af20926781ab13a25e0
Aryanmallik <aryanmallik2004@gmail.com>
2024-03-18 17:19:06 +0530

    draw

--- a.js
+++ a.js
@@ -3,98 +3,221 @@ const player = document.querySelector(".player .choice");
 const result = document.querySelector(".result");
 const playerScore = document.querySelector(".pscore");
 const cpuScore = document.querySelector(".cscore");
+let uscore = 0;
+let cscore = 0;
+let gameOver = false;
+
 function compinp() {
   let arr = ["rock", "paper", "scissors"];
   const compval = arr[Math.floor(Math.random() * arr.length)];
-  console.log("comp-", compval);
-  if (compval == "rock") {
-    comp.innerHTML = `<img src="./Public/rock.png" height="200px">`;
-  }
-  if (compval == "paper") {
-    comp.innerHTML = `<img src="./Public/paper.png" height="200px">`;
-  }
-  if (compval == "scissors") {
-    comp.innerHTML = `<img src="./Public/scissor.png" height="200px">`;
-  }
+  comp.innerHTML = `<img src="./Public/${compval}2.png" width="150px" height="200px">`;
   return compval;
 }
 
+// function computeWinner(a, b) {
+//   console.log(a, b);
+//   if (a == b) {
+//     return 0;
+//   } else if (
+//     (a == "rock" && b == "paper") ||
+//     (a == "paper" && b == "scissors") ||
+//     (a == "scissors" && b == "rock")
+//   ) {
+//     return 1;
+//   } else {
+//     return 2;
+//   }
+// }
 function computeWinner(a, b) {
-  if (a === b) {
+  if (a == b) {
     return 0;
-  } else if (
-    (a == "rock" && b == "paper") ||
-    (a == "paper" && b == "scissors") ||
-    (a == "scissors" && b == "rock")
-  ) {
+  } else if (a == "rock" && b == "paper") {
     return 1;
-  } else {
+  } else if (a == "rock" && b == "scissors") {
     return 2;
+  } else if (a == "paper" && b == "rock") {
+    return 2;
+  } else if (a == "paper" && b == "scissors") {
+    return 1;
+  } else if (a == "scissors" && b == "paper") {
+    return 2;
+  } else if (a == "scissors" && b == "rock") {
+    return 1;
   }
 }
 
-function computeScore() {
-  let uscore = 0;
-  let cscore = 0;
-  let gameOver = 0;
-  const buttons = document.querySelectorAll(".ch");
-  buttons.forEach((button) => {
-    button.addEventListener("click", function () {
-      let a = this.value;
-      console.log("user-", a);
-      if (a == "rock") {
-        player.innerHTML = `<img src="./Public/rock.png" height="200px">`;
-      }
-      if (a == "paper") {
-        player.innerHTML = `<img src="./Public/paper.png" height="200px">`;
-      }
-      if (a == "scissors") {
-        player.innerHTML = `<img src="./Public/scissor.png" height="200px">`;
-      }
-      if (!gameOver) {
-        let b = compinp();
-        let val = computeWinner(a, b);
-
-        if (val === 0) {
-          console.log("draw");
-          result.innerHTML = "draw";
-        } else if (val == 1) {
-          cscore++;
-          result.innerHTML = "you lose";
-        } else if (val == 2) {
-          uscore++;
-          result.innerHTML = "you win";
-        }
-        playerScore.innerHTML = uscore;
-        cpuScore.innerHTML = cscore;
-        console.log("user-", uscore, "comp-", cscore);
-        if (uscore == 5) {
-          console.log("user win");
-          result.innerHTML =
-            "game over you win<br/><br/>click one of the options to restart";
-          gameOver = true;
-        } else if (cscore == 5) {
-          result.innerHTML =
-            "game over you lose<br/><br/>click one of the options to restart";
-          gameOver = true;
-        }
-      }
-      if (gameOver) {
-        buttons.forEach((button) => {
-          button.addEventListener("click", function () {
-            uscore = 0;
-            cscore = 0;
-            gameOver = 1;
-            playerScore.innerHTML = uscore;
-            cpuScore.innerHTML = cscore;
-            result.innerHTML = "";
-            player.innerHTML = ``;
-            comp.innerHTML = ``;
-          });
-        });
-      }
-    });
-  });
+function handleGameOver(winner) {
+  gameOver = true;
+  if (winner === "player") {
+    result.innerHTML = "game over you win<br/><br/>click any button to restart";
+  } else {
+    result.innerHTML =
+      "game over you lose<br/><br/>click any button to restart";
+  }
 }
 
-computeScore();
+function handleMove(choice) {
+  if (!gameOver) {
+    player.innerHTML = `<img src="./Public/${choice}2.png" width="150px" height="200px">`;
+    let b = compinp();
+    let val = computeWinner(choice, b);
+    console.log(val);
+    if (val == 0) {
+      console.log("draw");
+      result.innerHTML = "draw";
+    } else if (val == 1) {
+      cscore++;
+      result.innerHTML = "you lose";
+    } else if (val == 2) {
+      uscore++;
+      result.innerHTML = "you win";
+    }
+    playerScore.innerHTML = uscore;
+    cpuScore.innerHTML = cscore;
+    console.log("user-", uscore, "comp-", cscore);
+
+    if (uscore == -1) {
+      console.log("user win");
+      handleGameOver("player");
+    } else if (cscore == -1) {
+      handleGameOver("computer");
+    }
+  }
+}
+
+function handleButtonClick() {
+  if (!gameOver) {
+    const choice = this.value;
+    handleMove(choice);
+  } else {
+    location.reload();
+  }
+}
+
+function handleKeyDown(event) {
+  if (!gameOver) {
+    let key;
+    switch (event.key) {
+      case "m":
+        key = "rock";
+        break;
+      case "k":
+        key = "paper";
+        break;
+      case "a":
+        key = "scissors";
+        break;
+      default:
+        return;
+    }
+    handleMove(key);
+  } else {
+    location.reload();
+  }
+}
+const buttons = document.querySelectorAll(".ch");
+buttons.forEach((button) => {
+  button.addEventListener("click", handleButtonClick);
+});
+
+document.addEventListener("keydown", handleKeyDown);
+// const comp = document.querySelector(".comp .choice");
+// const player = document.querySelector(".player .choice");
+// const result = document.querySelector(".result");
+// const playerScore = document.querySelector(".pscore");
+// const cpuScore = document.querySelector(".cscore");
+// function compinp() {
+//   let arr = ["rock", "paper", "scissors"];
+//   const compval = arr[Math.floor(Math.random() * arr.length)];
+//   console.log("comp-", compval);
+//   if (compval == "rock") {
+//     comp.innerHTML = `<img src="./Public/rock.png" height="200px">`;
+//   }
+//   if (compval == "paper") {
+//     comp.innerHTML = `<img src="./Public/paper.png" height="200px">`;
+//   }
+//   if (compval == "scissors") {
+//     comp.innerHTML = `<img src="./Public/scissor.png" height="200px">`;
+//   }
+//   return compval;
+// }
+
+// function computeWinner(a, b) {
+//   if (a === b) {
+//     return 0;
+//   } else if (
+//     (a == "rock" && b == "paper") ||
+//     (a == "paper" && b == "scissors") ||
+//     (a == "scissors" && b == "rock")
+//   ) {
+//     return 1;
+//   } else {
+//     return 2;
+//   }
+// }
+
+// function computeScore() {
+//   let uscore = 0;
+//   let cscore = 0;
+//   let gameOver = false;
+//   const buttons = document.querySelectorAll(".ch");
+//   buttons.forEach((button) => {
+//     button.addEventListener("click", function () {
+//       let a = this.value;
+//       console.log("user-", a);
+//       if (a == "rock") {
+//         player.innerHTML = `<img src="./Public/rock.png" height="200px">`;
+//       }
+//       if (a == "paper") {
+//         player.innerHTML = `<img src="./Public/paper.png" height="200px">`;
+//       }
+//       if (a == "scissors") {
+//         player.innerHTML = `<img src="./Public/scissor.png" height="200px">`;
+//       }
+//       if (!gameOver) {
+//         let b = compinp();
+//         let val = computeWinner(a, b);
+
+//         if (val === 0) {
+//           console.log("draw");
+//           result.innerHTML = "draw";
+//         } else if (val == 1) {
+//           cscore++;
+//           result.innerHTML = "you lose";
+//         } else if (val == 2) {
+//           uscore++;
+//           result.innerHTML = "you win";
+//         }
+//         playerScore.innerHTML = uscore;
+//         cpuScore.innerHTML = cscore;
+//         console.log("user-", uscore, "comp-", cscore);
+//         if (uscore == 5) {
+//           console.log("user win");
+//           result.innerHTML =
+//             "game over you win<br/><br/>click one of the options to restart";
+//           gameOver = true;
+//         } else if (cscore == 5) {
+//           result.innerHTML =
+//             "game over you lose<br/><br/>click one of the options to restart";
+//           gameOver = true;
+//         }
+//       }
+//       if (gameOver) {
+//         buttons.forEach((button) => {
+//           button.addEventListener("click", function () {
+//             uscore = 0;
+//             cscore = 0;
+//             gameOver = false;
+//             playerScore.innerHTML = uscore;
+//             cpuScore.innerHTML = cscore;
+//             result.innerHTML = "";
+//             player.innerHTML = ``;
+//             comp.innerHTML = ``;
+//             location.reload();
+//           });
+//         });
+//       }
+//     });
+//   });
+// }
+// computeScore();
